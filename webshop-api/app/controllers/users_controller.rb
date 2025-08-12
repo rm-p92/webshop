4ac044394e0a8535298b0dd1b84d10df21f4d8e6
rm-p92 <jose.roy.miana@p92.com>
2025-08-12 15:54:47 +0800

    create-react to vite-react

---
+++ webshop-api/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            token = issue_token(@user)
+            render json: { user: @user, token: token }, status: :created
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+end
+
--- webshop-api/config/routes.rb
+++ webshop-api/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # resources :users
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  get "/authorized", to: "sessions#show"
+  get "/dashboard", to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
